0bce86de5887c91debdaf154268ea6f697f3d950
Wei Zhao <zhaowei@mit.edu>
2013-04-14 17:22:51 -0400

    added homepage and restaurant filter page

---
+++ Javascripts/res_filter.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Wei
+ * Date: 4/14/13
+ * Time: 3:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function()
+{
+    $("address").focus(function(){
+        $(this).text("hello");
+    })
+
+});
