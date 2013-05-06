56d39709e5639f741f9db3a43596039da28c85b8
Evan <evan@evan-laptop.(none)>
2013-05-06 01:02:48 -0400

    hopefully it's good

--- Javascripts/sessiondata.js
+++ Javascripts/sessiondata.js
@@ -1,9 +1,10 @@
 var RestSelect = Parse.Object.extend("RestSelect");
+var DishesSelect = Parse.Object.extend("DishesSelect");
 
 //create a rest_select object
 //bind it to a selection index
 //save to the cloud
-function create_rest_select(index){
+function create_rest_select(index, callback){
   var user = Parse.User.current();
   var rest_select = new RestSelect();
   rest_select.set("user", user);
@@ -12,6 +13,7 @@ function create_rest_select(index){
     {
       success:function(rest_select){
         //console.log("saved to cloud OMG!");
+        callback();
       },
       error:function(rest_select,error){
         //console.log("failed? impossible");
@@ -24,7 +26,7 @@ function create_rest_select(index){
 //Parse cloud
 //we make sure we destroy all the old pushes here as well, since only 1 of this
 //information should exist at a time.
-function push_restaurant_index(index){
+function push_restaurant_index(index, callback){
   var rest_query = new Parse.Query(RestSelect);
   var user = Parse.User.current();
   rest_query.equalTo("user", user);
@@ -32,8 +34,8 @@ function push_restaurant_index(index){
   //we first try to find old ones
   rest_query.find({
     success: function(rest_selects){
-      //console.log("size of all the old entrieS");
-      //console.log(rest_selects.length);
+      console.log("size of all the old entrieS");
+      console.log(rest_selects.length);
       //remove the old ones
       for (var i = 0; i < rest_selects.length; i++) {
         rest_selects[i].destroy({
@@ -44,11 +46,11 @@ function push_restaurant_index(index){
         })
       }
       //add new one
-      create_rest_select(index);
+      create_rest_select(index,callback);
     },
     error: function(rest_select, error){
       //add new one
-      create_rest_select(index);
+      create_rest_select(index,callback);
     }
   });
 
@@ -67,6 +69,77 @@ function pull_restaurant_index(call_back){
       call_back(select);
     },
     error: function(rest_select, error){
+    }
+  });
+}
+
+//create a dishes_select object
+//bind it to a dish selection
+//save to the cloud
+function create_dish_select(dish_list, callback){
+  var user = Parse.User.current();
+  var dish_select = new DishesSelect();
+  dish_select.set("user", user);
+  dish_select.set("dishes", dish_list);
+  dish_select.save(null,
+    {
+      success:function(dish_select){
+        //console.log("saved to cloud OMG!");
+        callback();
+      },
+      error:function(dish_select,error){
+        //console.log("failed? impossible");
+      }
+    }
+  );
+}
+
+//push a dishes seelction for the user in the current session to the
+//Parse cloud
+//we make sure we destroy all the old pushes here as well, since only 1 of this
+//information should exist at a time.
+function push_dish_select(a_list, callback){
+  var dish_query = new Parse.Query(DishesSelect);
+  var user = Parse.User.current();
+  dish_query.equalTo("user", user);
+
+  //we first try to find old ones
+  dish_query.find({
+    success: function(dish_selects){
+      console.log("size of all the old entrieS");
+      console.log(dish_selects.length);
+      //remove the old ones
+      for (var i = 0; i < dish_selects.length; i++) {
+        dish_selects[i].destroy({
+          success: function(myObject) {
+          },
+          error: function(myObject, error) {
+          }
+        })
+      }
+      //add new one
+      create_dish_select(a_list,callback);
+    },
+    error: function(dish_select, error){
+      //add new one
+      create_dish_select(a_list,callback);
+    }
+  });
+}
+
+//pull the dishes selection for the current user
+function pull_dishes_list(call_back){
+  var user = Parse.User.current();
+  var dish_query = new Parse.Query(DishesSelect);
+  
+  dish_query.equalTo("user", user).first({
+    success: function(dish_select){
+      //console.log("found old entry for dish_query of this user");
+      //console.log(dish_select);
+      var select = dish_select.get("dishes");
+      call_back(select);
+    },
+    error: function(dish_select, error){
     }
   });
 }
--- Javascripts/summary.js
+++ Javascripts/summary.js
@@ -1,9 +1,75 @@
-$(document).ready(function()
-{
-      $("#print_btn").click(function(){
-           windows.print();
-        });
+function update_restaurant_info(restaurant){
+  $("#rest_name").text(restaurant.name); 
+  $("#rest_addr").text(restaurant.addr); 
+  $("#rest_phone").text("phone: "+restaurant.phone); 
+  $("#rest_logo").attr("src", "menu_ingr_data/rest_picture/"+restaurant.logo);
 }
 
+function add_dish(dish_src){
+  var li_ele = $('<li/>', {
+    class: "sel",
+  });
+
+  li_ele.appendTo($("#selection"));
+
+  var name_add = $('<div/>', {
+              text: dish_src["name"],
+              class: "dish_name"
+  });
+
+  var img_cont = $('<div/>', {
+    class: "img_container",
+  });
+  var img_add = $('<img class="dish_img">').attr('width','300');
+  img_add.attr('src', 'menu_ingr_data/menu_picture/' + dish_src["image"]).appendTo(img_cont);
+
+  var ingr_add = $('<div/>', {
+              text: 'contains: '+
+                    String(dish_src["ingredients"]).replace(
+                    /_/g," ").replace(/,/g,", "),
+              class: "dish_content"
+  });
+
+  li_ele.append(name_add);
+  li_ele.append(img_cont);
+  li_ele.append(ingr_add);
+}
+
+function find_dish(rest_dish, dish_name){
+  for (var i = 0; i < rest_dish.length; i++){
+    var _dish_name = rest_dish[i].name;
+    if (_dish_name == dish_name){
+      return i;
+    }
+  }
+  return 0;
+}
+
+function callback1(dishes){
+  console.log(dishes);
+
+  var render_page = function (rest){
+    var restaurant = restaurants[rest];
+    update_restaurant_info(restaurant);
+
+    for (var i = 0; i < dishes.length; i++){
+      var dish_name = dishes[i];
+      var dish_idx = find_dish(restaurant.dishes, dish_name);
+      add_dish(restaurant["dishes"][dish_idx]);
+    }
+
+  }
+
+  pull_restaurant_index(render_page)
+}
+
+$(document).ready(function(){
+  pull_dishes_list(callback1);
+
+  $("#print_btn").click(
+    function(){window.print();}
+  );
+
+});
 
 
