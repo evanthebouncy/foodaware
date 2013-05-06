05ca332a07ab94ab9c2ca63f7dd8daba1b888f05
Wei Zhao <zhaowei@mit.edu>
2013-05-05 22:28:33 -0400

    navbar failed somehow

--- Javascripts/navbar.js
+++ Javascripts/navbar.js
@@ -2,7 +2,7 @@ Parse.initialize("NJaqeDB583JMB1TdWF2Tkpwkn9UbFsOOMhxhhPds", "69wSIovgcpBxqhsdhj
 
 // I'm pretty sure this is a mortal sin, but in prod we'll be using a
 // templating system so it's OK.
-var navBar = "<div class='navbar'> <div class='navbar-inner'> <a class='brand' href='home.html'>FoodAware</a> <ul class='nav'>  <li><a href='restriction_prefs.html'><i class='icon-info-sign'></i> Diet Profile</a></li>  <li><a href='filter.html'><i class='icon-glass'></i> Restaurant</a></li>  <li><a href='menu.html'><i class='icon-food'></i> Menu</a></li>  <li><a href='summary.html'><i class='icon-reorder'></i> Summary</a></li><li class='pull-right'><a href='login.html'><i class='icon-user'></i> <span id='navbar-username'>Log in</span></a></li> </ul> </div> </div>"
+var navBar = "<div class='navbar'><div class='navbar-inner'><a class='brand' href='home.html'>FoodAware</a><ul class='nav'><li><a href='restriction_prefs.html'><i class='icon-info-sign'></i> Diet Profile</a></li><li><a href='filter.html'><i class='icon-glass'></i> Restaurant</a></li></i> <span id='navbar-username'>Log in</span></a></li> </ul> </div> </div>"
 
 $(document).ready(function() {
     $navBar = $(navBar);
