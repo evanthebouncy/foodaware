00442d0eac3bf48a574d9551daec94e536140fb9
Kata <lightquake@amateurtopologist.com>
2013-05-08 05:04:23 -0400

    Add search bar to assist in picking out ingredients.

--- Javascripts/restriction_prefs.js
+++ Javascripts/restriction_prefs.js
@@ -4,6 +4,65 @@ function ucFirst(str) {
     });
 }
 
+
+function toIdentifier(str) {
+    return str.replace(/ /g, "_").toLowerCase();
+}
+
+
+function toDisplayName(str) {
+    return ucFirst(str.replace(/_/g, " "));
+}
+
+
+var ValenceFilterView = Parse.View.extend({
+    events: {
+        "submit": function(ev) { ev.preventDefault(); },
+        "change input": function() { console.log("hi"); }
+    },
+
+    initialize: function() {
+        _.bindAll(this, "render");
+        this.$el.html("<form><input><div class='well'></div></form>");
+        this.$el.find("form").addClass("form-search");
+        this.$input = this.$el.find("input");
+        this.$input.attr("placeholder", "Ingredient name")
+            .addClass("input-medium search-query")
+            .attr("type", "text")
+            .keyup(this.render);
+        this.$output = this.$el.find("div");
+        this.options.ingredientList.sort();
+        var self = this;
+        var valenceButtons = _.map(this.options.ingredientList, function(ingredient) {
+            var button = new ValenceButton({ model: self.model,
+                                             displayName: toDisplayName(ingredient)
+                                           });
+            button.render();
+            button.$el.hide();
+            return button;
+        });
+        this.valenceButtons = valenceButtons;
+        this.$output.append(_.map(valenceButtons, function(button) { return button.$el }));
+    },
+
+    render: function() {
+        var filterValue = this.$input.val().toLowerCase();;
+        console.log(filterValue);
+        _.each(this.valenceButtons, function(button) {
+            console.log(button);
+            if (filterValue && button.options.displayName.toLowerCase().indexOf(filterValue) > -1) {
+                button.$el.show();
+            } else {
+                button.$el.hide();
+            }
+        });
+
+
+        return this;
+    }
+});
+
+
 $(document).ready(function() {
     var user = Parse.User.current();
     var query = new Parse.Query(Settings);
@@ -21,7 +80,10 @@ $(document).ready(function() {
 // This actually sets up all the data entry options; we call it once
 // we have the existing settings.
 var setupEntrySystem = function(settings) {
+    var ingredientList = [];
+
     $.each(ingredientGroups, function(index, group) {
+        ingredientList = ingredientList.concat(ingredients[group]);
         var itemButtons = $.map(ingredients[group], function(itemName) {
             return new ValenceButton({displayName: ucFirst(itemName),
                                       model: settings
@@ -43,6 +105,11 @@ var setupEntrySystem = function(settings) {
 
     $("#valence-groups").append(preferenceValenceGroup.render().el)
         .append(restrictionValenceGroup.render().el);
+
+    var filter = new ValenceFilterView({ingredientList: ingredientList,
+                                       model: settings});
+    $("#search").append(filter.render().$el);
+
 
     // Set up automatic debounced saving.
     settings.on("change", _.debounce(function() {
